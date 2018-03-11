b10a4a99797e7cdd9ebfec0530fc09162c4cc8b0
Chris Zuber <shgysk8zer0@gmail.com>
2018-03-10 20:24:56 -0800

    More config updates
    - Remove Rollup
    - Update PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-	"map": true,
-	"plugins": {
-		"cssnano": null,
-		"postcss-cssnext": null,
-		"postcss-import": null,
-		"postcss-url": null
-	}
-}
+	"map": {inline: false},
+	"plugins": [
+		require("postcss-cssnext"),
+		require("postcss-import"),
+		require("postcss-url"),
+		require("postcss-calc"),
+		require("postcss-custom-properties"),
+		require("postcss-discard-comments")({removeAll: true}),
+		require("cssnano"),
+	]
+};
